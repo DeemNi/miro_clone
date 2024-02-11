1d4cc2e1d14ca07b3820568daa1ee08e64870e57
Nikita Demianov <hourlystory@gmail.com>
2024-02-11 21:01:31 +0200

    Create mutation
    add some server staff

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+
 import { ConvexClientProvider } from "@/providers/convex-client-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ConvexClientProvider>
+          <Toaster />
           {children}
         </ConvexClientProvider>
       </body>
