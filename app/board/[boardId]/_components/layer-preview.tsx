7d4da6bd54b91885a4021a34100bbe929d640c5f
Nikita Demianov <hourlystory@gmail.com>
2024-02-28 23:02:15 +0200

    realized a cirle, note, text, and other element creator
    have a text bug and selection bug

--- app/board/[boardId]/_components/layer-preview.tsx
+++ app/board/[boardId]/_components/layer-preview.tsx
@@ -4,6 +4,10 @@ import { useStorage } from "@/liveblocks.config";
 import { LayerType } from "@/types/canvas";
 import { memo } from "react";
 import { Rectangle } from "./rectangle";
+import { Ellipse } from "./ellipse";
+
+import { Text } from "./text";
+import { Note } from "./note";
 
 
 interface LayerPreviewProps {
@@ -25,26 +29,46 @@ export const LayerPreview = memo(({
     }
 
     switch (layer.type) {
+        case LayerType.Note:
+            return (
+                <Note
+                    id={id}
+                    layer={layer}
+                    onPointerDown={onLayerPointerDown}
+                    selectionColor={selectionColor}
+                />
+            )
+        case LayerType.Text:
+            return (
+                <Text
+                    id={id}
+                    layer={layer}
+                    onPointerDown={onLayerPointerDown}
+                    selectionColor={selectionColor}
+                />
+            )
+        case LayerType.Ellipse:
+            return (
+                <Ellipse
+                    id={id}
+                    layer={layer}
+                    onPointerDown={onLayerPointerDown}
+                    selectionColor={selectionColor}
+                />
+            )
         case LayerType.Rectangle:
             return (
-                <Rectangle 
+                <Rectangle
                     id={id}
                     layer={layer}
                     onPointerDown={onLayerPointerDown}
                     selectionColor={selectionColor}
                 />
             );
-        default: 
+        default:
             console.warn('Unknown layer type');
             return null;
     }
-
-
-    return (
-        <div>
-
-        </div>
-    )
 });
 
 LayerPreview.displayName = 'LayerPreview';
