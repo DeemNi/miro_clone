64af003190181f2c8758a5603cb0699c1cbe073f
Nikita Demianov <hourlystory@gmail.com>
2024-02-29 03:24:28 +0200

    Pencil & Drawing functionality will be added.
    END OF THE PROJECT

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,28 +1,28 @@
-import { Camera, Color, Layer, Point, Side, XYWH } from "@/types/canvas";
+import { Camera, Color, Layer, LayerType, PathLayer, Point, Side, XYWH } from "@/types/canvas";
 import { type ClassValue, clsx } from "clsx"
 import React from "react";
 import { twMerge } from "tailwind-merge"
 
-const COLORS = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
-'#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
-'#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
-'#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
-'#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
-'#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
-'#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
-'#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
-'#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
-'#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
+const COLORS = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
+  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+  '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',
+  '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+  '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC',
+  '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+  '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680',
+  '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+  '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3',
+  '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function connectionIdToColor(connectionId:number):string {
+export function connectionIdToColor(connectionId: number): string {
   return COLORS[connectionId % COLORS.length];
 };
 
-export function pointerEventToCanvasPoint (
+export function pointerEventToCanvasPoint(
   e: React.PointerEvent,
   camera: Camera,
 ) {
@@ -32,84 +32,145 @@ export function pointerEventToCanvasPoint (
   }
 }
 
-export function colorToCss(color: Color){
-  return `#${color.r.toString(16).padStart(2,"0")}${color.g.toString(16).padStart(2,"0")}${color.b.toString(16).padStart(2,"0")}`
+export function colorToCss(color: Color) {
+  return `#${color.r.toString(16).padStart(2, "0")}${color.g.toString(16).padStart(2, "0")}${color.b.toString(16).padStart(2, "0")}`
 }
 
 export function resizeBounds(
   bounds: XYWH,
   corner: Side,
-  point:Point
-  ): XYWH {
-    const result = {
-      x: bounds.x,
-      y: bounds.y,
-      width: bounds.width,
-      height: bounds.height,
-    };
-
-    if((corner & Side.Left) === Side.Left){
-      result.x = Math.min(point.x, bounds.x + bounds.width);
-      result.width = Math.abs(bounds.x + bounds.width - point.x);
-    }
-
-    if((corner & Side.Right) === Side.Right){
-      result.x = Math.min(point.x, bounds.x);
-      result.width = Math.abs(point.x - bounds.x);
-    }
-
-    if((corner & Side.Top) === Side.Top){
-      result.y = Math.min(point.y, bounds.y + bounds.height);
-      result.height = Math.abs(bounds.y + bounds.height - point.y);
-    }
-
-    if((corner & Side.Bottom) === Side.Bottom){
-      result.y = Math.min(point.y,  bounds.y);
-      result.height = Math.abs(point.y - bounds.y);
-    }
-
-    return result;
+  point: Point
+): XYWH {
+  const result = {
+    x: bounds.x,
+    y: bounds.y,
+    width: bounds.width,
+    height: bounds.height,
   };
 
-  export function findIntersectingLayersWithRectangle(
-    layerIds: readonly string[],
-    layers: ReadonlyMap<string, Layer>,
-    a: Point,
-    b: Point,
-  ) { 
-    const rect = {
-      x: Math.min(a.x, b.x),
-      y: Math.min(a.y, b.y),
-      width: Math.abs(a.x-b.x),
-      height: Math.abs(a.y - b.y)
-    };
-
-    const ids = [];
-
-    for(const layerId of layerIds){
-      const layer = layers.get(layerId);
-
-      if (layer == null) {
-        continue;
-      }
-
-      const {x, y, height, width} = layer;    
-    
-      if (
-        rect.x + rect.width > x &&
-        rect.x < x + width && 
-        rect.y + rect.height > y &&
-        rect.y < y + height
-      ) {
-        ids.push(layerId);
-      }
-    }
-  
-    return ids;
-  };
-
-  export function getContrastingTextColor(color: Color){
-    const luminance = 0.299 * color.r + 0.587 * color.g + 0.114 * color.b;
-
-    return luminance > 182 ? 'black' : 'white';
+  if ((corner & Side.Left) === Side.Left) {
+    result.x = Math.min(point.x, bounds.x + bounds.width);
+    result.width = Math.abs(bounds.x + bounds.width - point.x);
   }
+
+  if ((corner & Side.Right) === Side.Right) {
+    result.x = Math.min(point.x, bounds.x);
+    result.width = Math.abs(point.x - bounds.x);
+  }
+
+  if ((corner & Side.Top) === Side.Top) {
+    result.y = Math.min(point.y, bounds.y + bounds.height);
+    result.height = Math.abs(bounds.y + bounds.height - point.y);
+  }
+
+  if ((corner & Side.Bottom) === Side.Bottom) {
+    result.y = Math.min(point.y, bounds.y);
+    result.height = Math.abs(point.y - bounds.y);
+  }
+
+  return result;
+};
+
+export function findIntersectingLayersWithRectangle(
+  layerIds: readonly string[],
+  layers: ReadonlyMap<string, Layer>,
+  a: Point,
+  b: Point,
+) {
+  const rect = {
+    x: Math.min(a.x, b.x),
+    y: Math.min(a.y, b.y),
+    width: Math.abs(a.x - b.x),
+    height: Math.abs(a.y - b.y)
+  };
+
+  const ids = [];
+
+  for (const layerId of layerIds) {
+    const layer = layers.get(layerId);
+
+    if (layer == null) {
+      continue;
+    }
+
+    const { x, y, height, width } = layer;
+
+    if (
+      rect.x + rect.width > x &&
+      rect.x < x + width &&
+      rect.y + rect.height > y &&
+      rect.y < y + height
+    ) {
+      ids.push(layerId);
+    }
+  }
+
+  return ids;
+};
+
+export function getContrastingTextColor(color: Color) {
+  const luminance = 0.299 * color.r + 0.587 * color.g + 0.114 * color.b;
+
+  return luminance > 182 ? 'black' : 'white';
+}
+
+export function penPointsToPathLayer(
+  points: number[][],
+  color: Color,
+): PathLayer {
+  if (points.length < 2) {
+    throw new Error('Cannot transform points with less than 2 point');
+  }
+
+  let left = Number.POSITIVE_INFINITY;
+  let top = Number.POSITIVE_INFINITY;
+  let right = Number.NEGATIVE_INFINITY;
+  let bottom = Number.NEGATIVE_INFINITY;
+
+  for (const point of points) {
+    const [x, y] = point;
+
+    if (left > x) {
+      left = x;
+    }
+
+    if (top > y) {
+      top = y;
+    }
+
+    if (right < x) {
+      right = x;
+    }
+
+    if (bottom < y) {
+      bottom = y;
+    }
+  }
+
+    return {
+      type: LayerType.Path,
+      x: left,
+      y: top,
+      width: right - left,
+      height: bottom - top,
+      fill: color,
+      points: points
+        .map(([x, y, pressure]) => [x - left, y - top, pressure])
+    };
+  }
+
+  export function getSvgPathFromStroke(stroke: number[][]) {
+    if (!stroke.length) return "";
+  
+    const d = stroke.reduce(
+      (acc, [x0, y0], i, arr) => {
+        const [x1, y1] = arr[(i + 1) % arr.length];
+        acc.push(x0, y0, (x0 + x1) / 2, (y0 + y1) / 2);
+        return acc;
+      },
+      ["M", ...stroke[0], "Q"]
+    );
+  
+    d.push("Z");
+    return d.join(" ");
+  };
